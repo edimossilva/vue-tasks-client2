8f24694051689e0f085cfb2bd438741d90d0c3de
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-31 01:30:09 +1200

    adding form items to login

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h1>Login</h1>
+    <div>
+      <label>Username:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label>Password:</label>
+      <input type="password" autocomplete="new-password" />
+    </div>
   </div>
 </template>
 
