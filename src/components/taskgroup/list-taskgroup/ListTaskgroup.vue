e96efef4301d8620da3801e7ff9678786e2d2493
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-31 04:44:11 +1200

    adding listtaskgroup component

---
+++ src/components/taskgroup/list-taskgroup/ListTaskgroup.vue
@@ -0,0 +1,8 @@
+// src/components/taskgroup/list-taskgroup/ListTaskgroup.vue
+<template>
+  <h1>List taskgroup</h1>
+</template>
+
+<script>
+export default {};
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 // ./source/routes.js
 import Login from "./components/login/Login.vue";
+import ListTaskgroup from "./components/taskgroup/list-taskgroup/ListTaskgroup.vue";
 
 export const routes = [
   {
     name: "login",
     path: "/login",
     component: Login
+  },
+  {
+    name: "listTaskgroup",
+    path: "/taskgroup",
+    component: ListTaskgroup
   }
 ];
